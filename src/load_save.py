1b2babc9edf0c30cdba6aca71e9ab93054995efa
tammavarapu <sambasiva.tammavarapu@gmail.com>
2021-11-12 19:22:17 +0530

    load_save completed

--- src/load_save.py
+++ src/load_save.py
@@ -1,4 +1,42 @@
-from src.utils.logger.appLogger import AppLogger as appLogger 
+from src.utils.logger.appLogger import appLogger as appLogger 
+from src.utils.all_utils import read_yaml
+import argparse
+import pandas as pd
+import os
+
+
+
+def get_data(config_path):
+    
+    """
+    This function is used to read the data from the config file.
+    """
+
+    try:
+        config = read_yaml(config_path)
+        # print(config)
+        
+        remote_data_path = config['data_source']
+        df = pd.read_csv(remote_data_path)
+        # print(df.head())
+
+        # save dataset in the local directory
+        # create path to directory
+
+        artifacts_dir = config['artifacts']['artifacts_dir']
+        local_data_path = config['artifacts']['input_data_dir']
+        data_file = config['artifacts']['data_file']
+
+        raw_local_dir_path = os.path.join(artifacts_dir, local_data_path)
+        raw_local_file_path = os.path.join(raw_local_dir_path, data_file)
+
+        df.to_csv(raw_local_file_path, sep=',', index=False)
+
+
+
+        
+    except Exception as e:
+        appLogger.error(e)
 
 
 
@@ -6,5 +44,11 @@ from src.utils.logger.appLogger import AppLogger as appLogger
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser
+    args = argparse.ArgumentParser()
+
+    args.add_argument("--config","-c", default ="config/config.yaml")
+    
+    parsed_args = args.parse_args()
+
+    get_data(config_path = parsed_args.config)
     
