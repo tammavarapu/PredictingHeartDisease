d01a336828246eda25fe74a43c9160ee592caeb0
tammavarapu <sambasiva.tammavarapu@gmail.com>
2021-11-12 09:50:58 +0530

    files created for moduler prgm

---
+++ src/utils/all_utils.py
@@ -0,0 +1,14 @@
+import yaml
+import os
+
+def read_yaml(path_to_yaml : str) -> dict:
+    with open(path_to_yaml, 'r') as yaml_file:
+        content = yaml.safe_load(yaml_file)
+
+    return content  
+
+
+def create_dir(dirs : list):
+    for dir_path in dirs:
+        os.makedirs(dir_path, exist_ok=True)
+        print(f"directory is created in {dir_path}")
